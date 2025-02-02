febe00675049055622c114e98b3518c8d7277e44
yasuyuky <yasuyuki.ymd@gmail.com>
2025-02-02 14:47:16 +0900

    Add webpack.config.js for bundling TypeScript files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    popup: "./src/popup/popup.ts",
+    background: "./src/background.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devtool: "source-map",
+};
